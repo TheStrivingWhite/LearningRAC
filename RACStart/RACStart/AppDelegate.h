a5ae98442753915e30ce87e7a7af969febb16a79
YY <1519098370@qq.com>
2019-03-21 10:22:12 +0800

    新建工程

---
+++ RACStart/RACStart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACStart
+//
+//  Created by 杨亚卓 on 2019/3/21.
+//  Copyright © 2019年 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RACStart/RACStart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACStart
+//
+//  Created by 杨亚卓 on 2019/3/21.
+//  Copyright © 2019年 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
